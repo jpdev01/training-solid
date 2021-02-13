4d6203c781cd7fde93ef622c85a80447a812516f
João Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-02-13 20:59:20 -0300

    Inserção de lógica para funcionário pleno + updates gerais

--- src/main/java/Simulator.java
+++ src/main/java/Simulator.java
@@ -1,6 +1,7 @@
 import calculator.Calculator;
 import employee.Employee;
 import employee.EmployeeInterface;
+import employee.EmployeePleno;
 import employee.EmployeeSenior;
 
 import java.util.ArrayList;
@@ -11,12 +12,12 @@ public class Simulator {
     public static void main(String[] args) {
         List<EmployeeInterface> list = new ArrayList<>();
         list.add(new Employee("Carlos", "12323", 1000f, 50f, 100f));
-        list.add(new EmployeeSenior("Pedro (Senior) ", "12323", 1000f, 50f, 100f));
-
+        list.add(new EmployeeSenior("Pedro ", "12323", 1000f, 50f, 100f));
+        list.add(new EmployeePleno("Paulo", "12323", 1000f, 50f, 100f));
         for (EmployeeInterface e : list)
         {
             Float salarioLiquido = e.getCalculator().salarioFinal(e);
-            System.out.println("O funcionário " + e.getName() + " receberá R$" + salarioLiquido + ".");
+            System.out.println("\nFuncionário: " + e.getName() + "\nCargo: " + e.getType() + "\nSalário Líquido atual: R$" + salarioLiquido + ".");
         }
 
     }
