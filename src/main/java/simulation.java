974908b2c1421cbb44d40bb260ee21c095c5b893
João Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-02-13 19:09:10 -0300

    simulação atualizada para um cenário onde o funcionário trabalhou apenas 50 horas

--- src/main/java/simulation.java
+++ src/main/java/simulation.java
@@ -1,3 +1,4 @@
+import calculator.Calculator;
 import employee.Employee;
 
 import java.util.ArrayList;
@@ -7,11 +8,12 @@ public class simulation {
 
     public static void main(String[] args) {
         List<Employee> list = new ArrayList<>();
-        list.add(new Employee("Carlos", "12323", 1000f, 100f, 100f));
+        list.add(new Employee("Carlos", "12323", 1000f, 50f, 100f));
 
         for (Employee e : list)
         {
-
+            Float salarioLiquido = new Calculator().salarioFinal(e);
+            System.out.println(salarioLiquido);
         }
 
     }
