7e9f1bfb770f45cf51e0a7a5528ff8d1e20fbf00
João Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-02-13 20:03:27 -0300

    Calculadora senior agora adiciona 20% a mais sobre o salario liquido de um funcionario senior

--- src/main/java/calculator/CalculatorSenior.java
+++ src/main/java/calculator/CalculatorSenior.java
@@ -6,6 +6,9 @@ public class CalculatorSenior implements CalculatorInterface{
     @Override
     public Float salarioFinal(EmployeeInterface func) {
         Float salarioLiquido = func.getWorkedHours() * (func.getSalario() / func.getTotalHours());
+
+        // logica de senior
+        salarioLiquido *= 1.2f;
         return salarioLiquido;
     }
 }
