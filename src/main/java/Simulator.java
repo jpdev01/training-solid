375f27d0f34b55a5069ff795b2e7d087c64cc1e2
João Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-02-13 23:02:10 -0300

    Implementação final das regras do principio i. + simulação

--- src/main/java/Simulator.java
+++ src/main/java/Simulator.java
@@ -3,6 +3,8 @@ import employee.Employee;
 import employee.EmployeeInterface;
 import employee.EmployeePleno;
 import employee.EmployeeSenior;
+import printer.Printer;
+import printer.SuperPrinter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +12,12 @@ import java.util.List;
 public class Simulator {
 
     public static void main(String[] args) {
+        simulatorEmployee();
+        simulatorPrinter();
+    }
+
+    public static void simulatorEmployee()
+    {
         List<EmployeeInterface> list = new ArrayList<>();
         list.add(new Employee("Carlos", "12323", 1000f, 50f, 100f));
         list.add(new EmployeeSenior("Pedro ", "12323", 1000f, 50f, 100f));
@@ -19,6 +27,18 @@ public class Simulator {
             Float salarioLiquido = e.getCalculator().salarioFinal(e);
             System.out.println("\nFuncionário: " + e.getName() + "\nCargo: " + e.getType() + "\nSalário Líquido atual: R$" + salarioLiquido + ".");
         }
+    }
+
+    public static void simulatorPrinter()
+    {
+        Printer hp = new Printer();
+        hp.print("Novo código penal 2021");
+
+        SuperPrinter epson = new SuperPrinter();
+        epson.print("Imprimindo todas as matérias para o curso de dev 2021");
+        epson.printDuplex("Conteúdos de todos os semestres");
+        epson.scan("Conteúdos antigos");
+        epson.sendFax("Comunicado aos professores");
 
     }
 }
